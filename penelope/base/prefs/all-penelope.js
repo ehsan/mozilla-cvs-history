a79069d4d925792c5b129d4db3b7505d52f63dd8
beckley%qualcomm.com <beckley%qualcomm.com>
2007-09-27 18:06:45 +0000

    Sources for the latest version of Penelope: 0.1a21.

--- penelope/base/prefs/all-penelope.js
+++ penelope/base/prefs/all-penelope.js
@@ -1,3 +1,10 @@
+// Override of TB settings
+pref("mail.purge.ask", false);
+pref("mail.prompt_purge_threshhold", true);
+pref("mail.purge_threshhold", 10000);
+pref("mail.useWhoColumn", true);
+
+// Penelope settings
 pref("penelope.selectedTextURL1", "Google,http://www.google.com/search?q=%s");
 pref("penelope.selectedTextURL2", "Dictionary,http://www.dictionary.com/search?q=%s");
 pref("penelope.selectedTextURL3", "Thesaurus,http://www.thesaurus.com/cgi-bin/search?config=roget&words=%s");
