f20f308ddea11e2a2c66d5d0f86ef92e0987ecfa
bugzilla%standard8.plus.com <bugzilla%standard8.plus.com>
2007-12-16 13:54:59 +0000

    Bug 408433 Fix address book xpcshell unit tests on windows. r/sr=Neil

--- mailnews/addrbook/test/unit/tail_cleanup_ab.js
+++ mailnews/addrbook/test/unit/tail_cleanup_ab.js
@@ -7,6 +7,24 @@ var abs = [ kPABData.fileName,
             kCABData.fileName ];
 
 try {
+  // Get the top-level directory
+  var rdf = Components.classes["@mozilla.org/rdf/rdf-service;1"]
+                      .getService(Components.interfaces.nsIRDFService);
+
+  var childNodes = rdf.GetResource("moz-abdirectory:///")
+                      .QueryInterface(Components.interfaces.nsIAbDirectory)
+                      .childNodes;
+
+  // and iterate through it, shutting down MDB databases where appropriate
+  while (childNodes.hasMoreElements()) {
+    var ab = childNodes.getNext();
+    if (ab instanceof Components.interfaces.nsIAbDirectory) {
+      var database = ab.database;
+      if (database)
+        database.forceClosed();
+    }
+  }
+
   // Now remove the directory
   if (profileDir.exists())
     profileDir.remove(true);
