81ed00311a8c63a0633743f9e65a19049913aecd
stuart.morgan%alumni.case.edu <stuart.morgan%alumni.case.edu>
2008-02-16 18:55:58 +0000

    Camino only - Rename ToolbarAdditions.{h,m} to NSToolbar+Utils.{h,m} (no bug). sr=mento

--- camino/src/extensions/NSToolbar+Utils.h
+++ camino/src/extensions/NSToolbar+Utils.h
@@ -38,12 +38,12 @@
 #import <AppKit/NSToolbar.h>
 
 @interface NSToolbar (CHToolbarCustomizableAdditions)
-- (BOOL) alwaysCustomizableByDrag;
-- (void) setAlwaysCustomizableByDrag:(BOOL) flag;
+- (BOOL)alwaysCustomizableByDrag;
+- (void)setAlwaysCustomizableByDrag:(BOOL)flag;
 
-- (BOOL) showsContextMenu;
-- (void) setShowsContextMenu:(BOOL) flag;
+- (BOOL)showsContextMenu;
+- (void)setShowsContextMenu:(BOOL)flag;
 
-- (unsigned int) indexOfFirstMovableItem;
-- (void) setIndexOfFirstMovableItem:(unsigned int) anIndex;
+- (unsigned int)indexOfFirstMovableItem;
+- (void)setIndexOfFirstMovableItem:(unsigned int)anIndex;
 @end
