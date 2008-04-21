23a16115bf9c81d56b06f0e72cb7bf85f13a4504
bhearsum%mozilla.com <bhearsum%mozilla.com>
2008-04-21 13:08:57 +0000

    bug 428308: tryserver changes for Buildbot 0.7.7. r=rhelmer, patch=me

--- tools/buildbot-configs/tryserver/buildbot/__init__.py
+++
@@ -1,2 +0,0 @@
-version = "0.7.5+"
-__path__.append("/tools/buildbot/lib/python2.5/site-packages/buildbot")
--- tools/buildbot-configs/tryserver/buildbot/process/__init__.py
+++
@@ -1,1 +0,0 @@
-__path__.append("/tools/buildbot/lib/python2.5/site-packages/buildbot/process")
