6e8a9dd330bd13fced18c1778ab0a48e45a65adf
bhearsum%mozilla.com <bhearsum%mozilla.com>
2009-01-08 21:02:37 +0000

    bug 471021: Try server Windows build machine and unit test slaves should have Unicode NSIS and have it in the PATH. r=me, patch=asasaki

--- tools/buildbotcustom/tryserver/env.py
+++ tools/buildbotcustom/tryserver/env.py
@@ -1,16 +1,12 @@
-from copy import copy
-
 from buildbotcustom.env import MozillaEnvironments
 
-MozillaEnvironments['tryserver'] = copy(
-    MozillaEnvironments['win32-ref-platform']
-)
-
-symbolServerVars = {
+MozillaEnvironments['tryserver'] = {
+    'CVS_RSH': 'ssh',
+    'MOZ_OBJDIR': "obj-firefox",
+    'TINDERBOX_OUTPUT': '1',
+    'MOZ_CRASHREPORTER_NO_REPORT': '1',
     'SYMBOL_SERVER_HOST': 'build.mozilla.org',
     'SYMBOL_SERVER_USER': 'trybld',
     'SYMBOL_SERVER_PATH': '/symbols/windows',
     'SYMBOL_SERVER_SSH_KEY': "$ENV{HOME}/.ssh/id_dsa"
 }
-
-MozillaEnvironments['tryserver'].update(symbolServerVars)
