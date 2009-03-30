806e60ccde111798b62d2f866e78468f8a83c931
reed%reedloden.com <reed%reedloden.com>
2009-03-30 01:34:53 +0000

    Allow for excess whitespace between the old and new files (follow-up to comment made in bug 485238).

--- tools/cvsmgmt/bulk-copy.pl
+++ tools/cvsmgmt/bulk-copy.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 while (my $line = <STDIN>) {
-    if ($line =~ /^(\S+)\s(\S+)$/) {
+    if ($line =~ /^(\S+)\s+(\S+)$/) {
         print "Copying $1 -> $2 ...\n";
         system("/opt/cvsmgmt/copy-cvs-file.pl -d $1,v $2,v");
     }
