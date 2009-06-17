9508e06716d747eff0850f18e29e0ef15dcd7cd4
kaie%kuix.de <kaie%kuix.de>
2009-06-17 23:26:41 +0000

    Bug 496446, Deliver new roots for Firefox 3.0.x (NSS_3_12_2_WITH_CKBI_1_75_RTM) r=nelson a1.9.0.12=samuel.sidler

--- client.mk
+++ client.mk
@@ -411,7 +411,7 @@ MODULES_all :=                                  \
 # and commit this file on that tag.
 #MOZ_CO_TAG          = <tag>
 NSPR_CO_TAG          = NSPR_4_7_5_RTM
-NSS_CO_TAG           = NSS_3_12_2_WITH_CKBI_1_73_RTM
+NSS_CO_TAG           = NSS_3_12_2_WITH_CKBI_1_75_RTM
 LDAPCSDK_CO_TAG      = LDAPCSDK_6_0_3_CLIENT_BRANCH
 LOCALES_CO_TAG       =
 
