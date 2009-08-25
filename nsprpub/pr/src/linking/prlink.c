e5d9d51c7aea8b4731dab0c51651151846eee230
julien.pierre.boogz%sun.com <julien.pierre.boogz%sun.com>
2009-08-25 21:29:44 +0000

    Fix for bug 511315. Add support for LOAD_WITH_ALTERED_SEARCH_PATH flag to PR_LoadLibraryWithFlags . r=wtc

--- nsprpub/pr/src/linking/prlink.c
+++ nsprpub/pr/src/linking/prlink.c
@@ -752,7 +752,9 @@ pr_LoadLibraryByPathname(const char *name, PRIntn flags)
     {
     HINSTANCE h;
 
-    h = LoadLibraryW(wname);
+    h = LoadLibraryExW(wname, NULL,
+                       (flags & PR_LD_ALT_SEARCH_PATH) ?
+                       LOAD_WITH_ALTERED_SEARCH_PATH : 0);
     if (h == NULL) {
         oserr = _MD_ERRNO();
         PR_DELETE(lm);
