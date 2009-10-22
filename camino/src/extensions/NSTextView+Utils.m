b3361d6fd1297e328d873a149e6f7ecfe3e0e7a3
alqahira%ardisson.org <alqahira%ardisson.org>
2009-10-22 17:25:22 +0000

    Bug 523858 - froodian should be the Initial Developer of NSTextView+Utils, since he was the author of the code moved to these files. r/sr=smorgan

--- camino/src/extensions/NSTextView+Utils.m
+++ camino/src/extensions/NSTextView+Utils.m
@@ -15,8 +15,8 @@
  *
  * The Original Code is Camino code.
  *
- * The Initial Developer of the Original Code is The Mozilla Foundation.
- * Portions created by the Initial Developer are Copyright (C) 2007
+ * The Initial Developer of the Original Code is Ian Leue.
+ * Portions created by the Initial Developer are Copyright (C) 2006
  * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s):
