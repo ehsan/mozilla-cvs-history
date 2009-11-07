7b9738e41618d3f7a2eb715391a4092d3bc414d4
wtc%netscape.com <wtc%netscape.com>
2009-11-07 01:13:22 +0000

    Bug 131826: backed out the zlib 1.1.4 upgrade because the signtool tests failed. Modified Files: 	README adler32.c compress.c crc32.c deflate.c deflate.h 	example.c gzio.c infblock.c infblock.h infcodes.c infcodes.h 	inffast.c inffast.h inflate.c inftrees.c inftrees.h infutil.c 	infutil.h minigzip.c trees.c uncompr.c zconf.h zlib.h zutil.c 	zutil.h

--- security/nss/lib/zlib/adler32.c
+++ security/nss/lib/zlib/adler32.c
@@ -1,9 +1,9 @@
 /* adler32.c -- compute the Adler-32 checksum of a data stream
- * Copyright (C) 1995-2002 Mark Adler
+ * Copyright (C) 1995-1996 Mark Adler
  * For conditions of distribution and use, see copyright notice in zlib.h 
  */
-
-/* @(#) $Id: adler32.c,v 1.2 2009/11/07 01:13:07 wtc%netscape.com Exp $ */
+/* This file was modified since it was taken from the zlib distribution */
+/* $Id: adler32.c,v 1.3 2009/11/07 01:13:08 wtc%netscape.com Exp $ */
 
 #include "zlib.h"
 
@@ -18,7 +18,7 @@
 #define DO16(buf)   DO8(buf,0); DO8(buf,8);
 
 /* ========================================================================= */
-uLong ZEXPORT adler32(adler, buf, len)
+PR_PUBLIC_API(uLong) adler32(adler, buf, len)
     uLong adler;
     const Bytef *buf;
     uInt len;
