539bb679b31534ef30d24257d6d4f0ad218945b7
nrthomas%gmail.com <nrthomas%gmail.com>
2010-01-13 21:44:58 +0000

    Bug 539504, unthrottle 3.0.17 -> 3.5.7 major update (and setup up 3.0.18 throttling), p=joduinn, r=morgamic

--- webtools/aus/xml/inc/config-dist.php
+++ webtools/aus/xml/inc/config-dist.php
@@ -87,7 +87,7 @@ define('THROTTLE_LOGGING',false);
 // );
 $productThrottling = array(
      'Firefox' => array(
-         '3.0.17' => 0
+         '3.0.18' => 0
      ),
     'Thunderbird' => array(
          '2.0.0.23' => 0
@@ -107,7 +107,7 @@ $productThrottling = array(
 // In this example, 3.0.11 with channel names that match these channels will not
 // be throttled unless there is a global throttle enabled.
 $throttleExceptions = array(
-    '3.0.17' => array (
+    '3.0.18' => array (
         'betatest',
         'releasetest'
     )
