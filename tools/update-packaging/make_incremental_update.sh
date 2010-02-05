6a651fb5cbb8b6af1082658e7cffd083fbf239d8
nrthomas%gmail.com <nrthomas%gmail.com>
2010-02-05 03:10:57 +0000

    Bug 535090 - backout forcing fix for linux users

--- tools/update-packaging/make_incremental_update.sh
+++ tools/update-packaging/make_incremental_update.sh
@@ -48,7 +48,7 @@ if [ $# = 0 ]; then
   exit 1
 fi
 
-requested_forced_updates='components/components.list Contents/MacOS/components/components.list mozilla-runtime libmozsqlite3.so'
+requested_forced_updates='components/components.list Contents/MacOS/components/components.list'
 
 while getopts "hf:" flag
 do
