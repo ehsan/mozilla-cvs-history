769fb4642a0697567df201e5212a01000a585027
ccooper%deadsquid.com <ccooper%deadsquid.com>
2010-03-18 16:48:48 +0000

    b=507024 r=morgamic - Add Fennec version string to the list of products in AUS

--- webtools/aus/xml/inc/config-dist.php
+++ webtools/aus/xml/inc/config-dist.php
@@ -153,6 +153,10 @@ $productBranchVersions = array(
         '0.8*'    =>  'branch',
         '0.9*'    =>  'branch',
         '0.6a1'   =>  'trunk'
+    ),
+    '{a23983c0-fd0e-11dc-95ff-0800200c9a66}' =>  array(
+        '*_1.9.2*' => 'mozilla-1.9.2',
+        '*_1.9.3*' => 'mozilla-central'
     )  // Add this for tests: 'Synthetic'   => array('1.5.0.*' => '1.5.0.x')
 );
 
