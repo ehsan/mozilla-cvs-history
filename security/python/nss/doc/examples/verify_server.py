4ccac708532b2c9adaa42f785de00147f96b05d0
jdennis%redhat.com <jdennis%redhat.com>
2010-03-24 16:53:07 +0000

    Fix several problems in auth_certificate_callback(). Add utility to print out SSLUsage flags. Set the auth_certificate_callback in the client to verify the server, it was a big problem that had been failing to verify the server cert. Add new utility to verify a server based on the auth_certificate_callback().

--- security/python/nss/doc/examples/verify_server.py
+++ security/python/nss/doc/examples/verify_server.py
@@ -48,26 +48,21 @@ import nss.nss as nss
 import nss.ssl as ssl
 
 # -----------------------------------------------------------------------------
-NO_CLIENT_CERT             = 0
-REQUEST_CLIENT_CERT_ONCE   = 1
-REQUIRE_CLIENT_CERT_ONCE   = 2
-REQUEST_CLIENT_CERT_ALWAYS = 3
-REQUIRE_CLIENT_CERT_ALWAYS = 4
 
 # command line parameters, default them to something reasonable
-client = False
-server = False
 password = ''
-use_ssl = True
-client_cert_action = NO_CLIENT_CERT
-certdir = 'pki'
+certdir = '/etc/httpd/alias'
+#certdir = '/etc/pki/nssdb'
 hostname = os.uname()[1]
 nickname = hostname.split('.')[0]
-port = 1234
+port = 443
 
+request = '''\
+GET /index.html HTTP/1.0
 
+'''
 # -----------------------------------------------------------------------------
-# Utility Functions
+# Callback Functions
 # -----------------------------------------------------------------------------
 
 def cert_usage_str(cert_usage):
@@ -129,10 +124,6 @@ def cert_usage_str(cert_usage):
         usage_str += ' (plus unknown flags %#x)' % cert_usage
 
     return usage_str
-
-# -----------------------------------------------------------------------------
-# Callback Functions
-# -----------------------------------------------------------------------------
 
 def password_callback(slot, retry, password):
     if password: return password
@@ -204,6 +195,7 @@ def auth_certificate_callback(sock, check_sig, is_server, certdb):
     return cert_is_valid
 
 def client_auth_data_callback(ca_names, chosen_nickname, password, certdb):
+    print "client_auth_data_callback"
     cert = None
     if chosen_nickname:
         try:
@@ -232,170 +224,74 @@ def client_auth_data_callback(ca_names, chosen_nickname, password, certdb):
 # Client Implementation
 # -----------------------------------------------------------------------------
 
-def Client():
+def client():
     # Get the IP Address of our server
     net_addr = io.NetworkAddress(hostname, port)
-    if use_ssl:
-        sock = ssl.SSLSocket()
+    sock = ssl.SSLSocket()
 
-        # Set client SSL socket options
-        sock.set_ssl_option(ssl.SSL_SECURITY, True)
-        sock.set_ssl_option(ssl.SSL_HANDSHAKE_AS_CLIENT, True)
-        sock.set_hostname(hostname)
-        sock.reset_handshake(False) # FIXME: is this needed
+    # Set client SSL socket options
+    sock.set_ssl_option(ssl.SSL_SECURITY, True)
+    sock.set_ssl_option(ssl.SSL_HANDSHAKE_AS_CLIENT, True)
+    sock.set_hostname(hostname)
 
-        # Provide a callback which notifies us when the SSL handshake is complete
-        sock.set_handshake_callback(handshake_callback)
+    # Provide a callback which notifies us when the SSL handshake is
+    # complete
+    sock.set_handshake_callback(handshake_callback)
 
-        # Provide a callback to supply our client certificate info
-        sock.set_client_auth_data_callback(client_auth_data_callback, nickname,
-                                           password, nss.get_default_certdb())
+    # Provide a callback to supply our client certificate info
+    sock.set_client_auth_data_callback(client_auth_data_callback, nickname,
+                                       password, nss.get_default_certdb())
 
-        # Provide a callback to verify the servers certificate
-        sock.set_auth_certificate_callback(auth_certificate_callback,
-                                           nss.get_default_certdb())
-    else:
-        sock = io.Socket()
+    # Provide a callback to verify the servers certificate
+    sock.set_auth_certificate_callback(auth_certificate_callback,
+                                       nss.get_default_certdb())
 
     print "client connecting to: %s" % (net_addr)
     sock.connect(net_addr)
 
-    # Talk to the server
+    sock.reset_handshake(False) # FIXME: is this needed
+
     try:
-        sock.send("Hello")
-        buf = sock.recv(1024)
-        if not buf:
-            print "client lost connection"
-            return
-        print "client received: %s" % (buf)
+        # Talk to the server
+        n_received = 0
+        sock.send(request)
+        while True:
+            buf = sock.recv(1024)
+            n_received += len(buf)
+            if not buf:
+                print "\nclient lost connection, received %d bytes" % (n_received)
+                break
     except Exception, e:
         print e.strerror
         sock.shutdown()
         return
 
-    # End of (simple) protocol session?
-    if buf == 'Goodbye':
-        sock.shutdown()
-        return
-
-# -----------------------------------------------------------------------------
-# Server Implementation
-# -----------------------------------------------------------------------------
-
-def Server():
-    # Perform basic SSL server configuration
-    ssl.set_default_cipher_pref(ssl.SSL_RSA_WITH_NULL_MD5, True)
-    ssl.config_server_session_id_cache()
-
-    # Get our certificate and private key
-    server_cert = nss.find_cert_from_nickname(nickname, password)
-    priv_key = nss.find_key_by_any_cert(server_cert, password)
-    server_cert_kea = server_cert.find_kea_type();
-
-    print "server cert:\n%s" % server_cert
-
-    # Setup an IP Address to listen on any of our interfaces
-    net_addr = io.NetworkAddress(io.PR_IpAddrAny, port)
-
-    if use_ssl:
-        sock = ssl.SSLSocket()
-
-        # Set server SSL socket options
-        sock.set_pkcs11_pin_arg(password)
-        sock.set_ssl_option(ssl.SSL_SECURITY, True)
-        sock.set_ssl_option(ssl.SSL_HANDSHAKE_AS_SERVER, True)
-
-        # If we're doing client authentication then set it up
-        if client_cert_action >= REQUEST_CLIENT_CERT_ONCE:
-            sock.set_ssl_option(ssl.SSL_REQUEST_CERTIFICATE, True)
-        if client_cert_action == REQUIRE_CLIENT_CERT_ONCE:
-            sock.set_ssl_option(ssl.SSL_REQUIRE_CERTIFICATE, True)
-        sock.set_auth_certificate_callback(auth_certificate_callback, nss.get_default_certdb())
-
-        # Configure the server SSL socket
-        sock.config_secure_server(server_cert, priv_key, server_cert_kea)
-        sock.reset_handshake(True) # FIXME: is this needed?
-
-    else:
-        sock = io.Socket()
-
-    # Bind to our network address and listen for clients
-    sock.bind(net_addr)
-    print "listening on: %s" % (net_addr)
-    sock.listen()
-
-    while True:
-        # Accept a connection from a client
-        client_sock, client_addr = sock.accept()
-        if use_ssl:
-            client_sock.set_handshake_callback(handshake_callback)
-
-        print "client connect from: %s" % (client_addr)
-
-        while True:
-            try:
-                # Handle the client connection
-                buf = client_sock.recv(1024)
-                if not buf:
-                    print "server lost lost connection to %s" % (client_addr)
-                    break
-
-                print "server received: %s" % (buf)
-
-                client_sock.send("Goodbye")
-                client_sock.shutdown(io.PR_SHUTDOWN_RCV)
-                break
-            except Exception, e:
-                print e.strerror
-                break
-        break
-
     sock.shutdown()
-
+    return
 
 # -----------------------------------------------------------------------------
 
 usage_str = '''
--C --client     run as the client (default: %(client)s)
--S --server     run as the server (default: %(server)s)
 -d --certdir    certificate directory (default: %(certdir)s)
 -h --hostname   host to connect to (default: %(hostname)s)
 -n --nickname   certificate nickname (default: %(nickname)s)
 -w --password   certificate database password (default: %(password)s)
 -p --port       host port (default: %(port)s)
--e --encrypt    use SSL (default) (default: %(encrypt)s)
--E --noencrypt  don't use SSL (default: %(noencrypt)s)
--f --require_cert_once (default: %(require_cert_once)s)
--F --require_cert_always (default: %(require_cert_always)s)
--r --request_cert_once (default: %(request_cert_once)s)
--R --request_cert_always (default: %(request_cert_always)s)
--H --help
 ''' % {
-       'client'              : client,
-       'server'              : server,
        'certdir'             : certdir,
        'hostname'            : hostname,
        'nickname'            : nickname,
        'password'            : password,
        'port'                : port,
-       'encrypt'             : use_ssl is True,
-       'noencrypt'           : use_ssl is False,
-       'require_cert_once'   : client_cert_action == REQUIRE_CLIENT_CERT_ONCE,
-       'require_cert_always' : client_cert_action == REQUIRE_CLIENT_CERT_ALWAYS,
-       'request_cert_once'   : client_cert_action == REQUEST_CLIENT_CERT_ONCE,
-       'request_cert_always' : client_cert_action == REQUEST_CLIENT_CERT_ALWAYS,
        }
 
 def usage():
     print usage_str
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "Hd:h:n:w:p:CSeEfFrR",
+    opts, args = getopt.getopt(sys.argv[1:], "Hd:h:n:w:p:",
                                ["help", "certdir=", "hostname=",
                                 "nickname=", "password=", "port=",
-                                "client", "server", "encrypt", "noencrypt",
-                                "require_cert_once", "require_cert_always",
-                                "request_cert_once", "request_cert_always",
                                 ])
 except getopt.GetoptError:
     # print help information and exit:
@@ -414,48 +310,18 @@ for o, a in opts:
         password = a
     if o in ("-p", "--port"):
         port = int(a)
-    if o in ("-C", "--client"):
-        client = True
-    if o in ("-S", "--server"):
-        server = True
-    if o in ("-e", "--encrypt"):
-        use_ssl = True
-    if o in ("-E", "--noencrypt"):
-        use_ssl = False
-    if o in ("-f", "--require_cert_once"):
-        client_cert_action = REQUIRE_CLIENT_CERT_ONCE
-    if o in ("-F", "--require_cert_always"):
-        client_cert_action = REQUIRE_CLIENT_CERT_ALWAYS
-    if o in ("-r", "--request_cert_once"):
-        client_cert_action = REQUEST_CLIENT_CERT_ONCE
-    if o in ("-R", "--request_cert_always"):
-        client_cert_action = REQUEST_CLIENT_CERT_ALWAYS
     if o in ("-H", "--help"):
         usage()
         sys.exit()
 
-if client and server:
-    print "can't be both client and server"
-    sys.exit(1)
-if not (client or server):
-    print "must be one of client or server"
-    sys.exit(1)
-
 # Perform basic configuration and setup
-if certdir is None:
-    nss.nss_init_nodb()
-else:
+try:
     nss.nss_init(certdir)
+    ssl.set_domestic_policy()
+    nss.set_password_callback(password_callback)
+except Exception, e:
+    print >>sys.stderr, e.strerror
+    sys.exit(1)
 
-ssl.set_domestic_policy()
-nss.set_password_callback(password_callback)
-
-# Run as a client or as a server
-if client:
-    print "starting as client"
-    Client()
-
-if server:
-    print "starting as server"
-    Server()
+client()
 
