59ff2566278368f1ddd30425a0d18ec19c7ec1d8
nrthomas%gmail.com <nrthomas%gmail.com>
2010-06-22 23:15:37 +0000

    Bug 560551, remove throttling for Firefox 3.0.19 (to update people to 3.6.4), r=morgamic

--- webtools/aus/xml/inc/config-dist.php
+++ webtools/aus/xml/inc/config-dist.php
@@ -87,7 +87,6 @@ define('THROTTLE_LOGGING',false);
 // );
 $productThrottling = array(
      'Firefox' => array(
-         '3.0.19' => 0,
          '3.5.9' => 0,
          '3.5.10' => 0
      ),
@@ -109,10 +108,6 @@ $productThrottling = array(
 // In this example, 3.0.11 with channel names that match these channels will not
 // be throttled unless there is a global throttle enabled.
 $throttleExceptions = array(
-    '3.0.19' => array (
-        'betatest',
-        'releasetest'
-    ),
     '3.5.9' => array (
         'betatest',
         'releasetest',
