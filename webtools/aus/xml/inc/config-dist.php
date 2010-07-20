0060791eb02c017a703de1a99d85eb19b922f91d
bhearsum%mozilla.com <bhearsum%mozilla.com>
2010-07-20 17:51:36 +0000

    bug 576201: Tracking bug for build and release of Firefox 3.6.7 - [cvs] Re-throttle 3.0.19 updates. r=bhearsum, patch=rail

--- webtools/aus/xml/inc/config-dist.php
+++ webtools/aus/xml/inc/config-dist.php
@@ -87,6 +87,7 @@ define('THROTTLE_LOGGING',false);
 // );
 $productThrottling = array(
      'Firefox' => array(
+         '3.0.19' => 0,
          '3.5.11' => 0
      ),
      'Thunderbird' => array(
@@ -107,6 +108,10 @@ $productThrottling = array(
 // In this example, 3.0.11 with channel names that match these channels will not
 // be throttled unless there is a global throttle enabled.
 $throttleExceptions = array(
+    '3.0.19' => array (
+        'betatest',
+        'releasetest'
+    ),
     '3.5.11' => array (
         'betatest',
         'releasetest'
