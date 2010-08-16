e7ca34e8c976f2a39e0fd334d16a87a20dcc1cd6
wtc%google.com <wtc%google.com>
2010-08-16 18:19:02 +0000

    Bug 587234: Better error reporting for tiny DH keys in Server Key Exchange. Add a new error code SSL_ERROR_WEAK_SERVER_KEY for the dh_p size check in ssl3_HandleServerKeyExchange.  r=nelson. Modified Files: 	cmd/lib/SSLerrs.h lib/ssl/ssl3con.c lib/ssl/sslerr.h

--- security/nss/lib/ssl/ssl3con.c
+++ security/nss/lib/ssl/ssl3con.c
@@ -39,7 +39,7 @@
  * the terms of any one of the MPL, the GPL or the LGPL.
  *
  * ***** END LICENSE BLOCK ***** */
-/* $Id: ssl3con.c,v 1.144 2010/08/12 01:15:38 wtc%google.com Exp $ */
+/* $Id: ssl3con.c,v 1.145 2010/08/16 18:19:02 wtc%google.com Exp $ */
 
 #include "cert.h"
 #include "ssl.h"
@@ -5304,8 +5304,10 @@ ssl3_HandleServerKeyExchange(sslSocket *ss, SSL3Opaque *b, PRUint32 length)
     	if (rv != SECSuccess) {
 	    goto loser;		/* malformed. */
 	}
-	if (dh_p.len < 512/8)
+	if (dh_p.len < 512/8) {
+	    errCode = SSL_ERROR_WEAK_SERVER_KEY;
 	    goto alert_loser;
+	}
     	rv = ssl3_ConsumeHandshakeVariable(ss, &dh_g, 2, &b, &length);
     	if (rv != SECSuccess) {
 	    goto loser;		/* malformed. */
--- security/nss/lib/ssl/sslerr.h
+++ security/nss/lib/ssl/sslerr.h
@@ -36,7 +36,7 @@
  * the terms of any one of the MPL, the GPL or the LGPL.
  *
  * ***** END LICENSE BLOCK ***** */
-/* $Id: sslerr.h,v 1.11 2010/06/24 09:24:18 nelson%bolyard.com Exp $ */
+/* $Id: sslerr.h,v 1.12 2010/08/16 18:19:02 wtc%google.com Exp $ */
 #ifndef __SSL_ERR_H_
 #define __SSL_ERR_H_
 
@@ -200,6 +200,8 @@ SSL_ERROR_RENEGOTIATION_NOT_ALLOWED     = (SSL_ERROR_BASE + 112),
 SSL_ERROR_UNSAFE_NEGOTIATION            = (SSL_ERROR_BASE + 113),
 
 SSL_ERROR_RX_UNEXPECTED_UNCOMPRESSED_RECORD	= (SSL_ERROR_BASE + 114),
+
+SSL_ERROR_WEAK_SERVER_KEY               = (SSL_ERROR_BASE + 115),
 
 SSL_ERROR_END_OF_LIST	/* let the c compiler determine the value of this. */
 } SSLErrorCodes;
