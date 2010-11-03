bf8f7bc0e3fcfc9bec008f8890031f147ce1443d
bhearsum%mozilla.com <bhearsum%mozilla.com>
2010-11-03 19:14:19 +0000

    bug 509364: ship unthrottled 3.0.19 - > 3.6.12 MU on beta and release channels. r=catlee

--- webtools/aus/xml/inc/config-dist.php
+++ webtools/aus/xml/inc/config-dist.php
@@ -87,7 +87,6 @@ define('THROTTLE_LOGGING',false);
 // );
 $productThrottling = array(
      'Firefox' => array(
-         '3.0.19' => 0,
          '3.5.15' => 0
      )
 );
@@ -105,10 +104,6 @@ $productThrottling = array(
 // In this example, 3.0.11 with channel names that match these channels will not
 // be throttled unless there is a global throttle enabled.
 $throttleExceptions = array(
-    '3.0.19' => array (
-        'betatest',
-        'releasetest',
-    ),
     '3.5.15' => array (
         'betatest',
         'releasetest'
