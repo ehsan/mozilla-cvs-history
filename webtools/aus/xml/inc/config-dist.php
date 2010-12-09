4f9e6ebee51a30d2387d870e8949aa25569f4a7d
ccooper%deadsquid.com <ccooper%deadsquid.com>
2010-12-09 19:37:04 +0000

    b=613900 r=bhearsum - Unthrottle 3.5.15, throttle 3.5.16 v2: remove beta channel from list of exceptions

--- webtools/aus/xml/inc/config-dist.php
+++ webtools/aus/xml/inc/config-dist.php
@@ -87,7 +87,7 @@ define('THROTTLE_LOGGING',false);
 // );
 $productThrottling = array(
      'Firefox' => array(
-         '3.5.15' => 0
+         '3.5.16' => 0
      )
 );
 
@@ -104,10 +104,9 @@ $productThrottling = array(
 // In this example, 3.0.11 with channel names that match these channels will not
 // be throttled unless there is a global throttle enabled.
 $throttleExceptions = array(
-    '3.5.15' => array (
+    '3.5.16' => array (
         'betatest',
-        'releasetest',
-        'beta'
+        'releasetest'
     )
 );
 
