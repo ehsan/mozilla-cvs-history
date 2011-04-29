e31d1946fd0ce64c3043c4b11bc113362bc8a089
nrthomas%gmail.com <nrthomas%gmail.com>
2011-04-29 09:12:33 +0000

    Bug 653676, update throttling for 3.5.19 and 3.6.17, r=rail

--- webtools/aus/xml/inc/config-dist.php
+++ webtools/aus/xml/inc/config-dist.php
@@ -87,8 +87,8 @@ define('THROTTLE_LOGGING',false);
 // );
 $productThrottling = array(
      'Firefox' => array(
-         '3.5.18' => 0,
-         '3.6.16' => 0
+         '3.5.19' => 0,
+         '3.6.17' => 0
      )
 );
 
@@ -105,15 +105,13 @@ $productThrottling = array(
 // In this example, 3.0.11 with channel names that match these channels will not
 // be throttled unless there is a global throttle enabled.
 $throttleExceptions = array(
-    '3.5.18' => array (
+    '3.5.19' => array (
         'betatest',
-        'releasetest',
-	'beta'
+        'releasetest'
     ),
-    '3.6.16' => array (
+    '3.6.17' => array (
         'betatest',
-        'releasetest',
-	'beta'
+        'releasetest'
     )
 );
 
