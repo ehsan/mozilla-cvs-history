80ca8781725a34fd790a8f0c4eddae67c4e3b25e
cltbld <cltbld>
2011-12-20 04:24:41 +0000

    Bug 701054 - Tracking bug for build and release of Firefox/Fennec 9.0. p=rail, r=bhearsum

--- webtools/aus/xml/inc/config-dist.php
+++ webtools/aus/xml/inc/config-dist.php
@@ -88,6 +88,17 @@ define('THROTTLE_LOGGING',false);
 $productThrottling = array(
      'Firefox' => array(
          '3.6.25' => 0,
+         '4.0' => 0,
+         '4.0.1' => 0,
+         '5.0' => 0,
+         '5.0.1' => 0,
+         '6.0' => 0,
+         '6.0.1' => 0,
+         '6.0.2' => 0,
+         '7.0' => 0,
+         '7.0.1' => 0,
+         '8.0' => 0,
+         '8.0.1' => 0,
      )
 );
 
@@ -107,6 +118,61 @@ $throttleExceptions = array(
     '3.6.25' => array (
         'betatest',
         'releasetest'
+    ),
+    '4.0' => array (
+        'betatest',
+        'releasetest',
+        'beta',
+    ),
+    '4.0.1' => array (
+        'betatest',
+        'releasetest',
+        'beta',
+    ),
+    '5.0' => array (
+        'betatest',
+        'releasetest',
+        'beta',
+    ),
+    '5.0.1' => array (
+        'betatest',
+        'releasetest',
+        'beta',
+    ),
+    '6.0' => array (
+        'betatest',
+        'releasetest',
+        'beta',
+    ),
+    '6.0.1' => array (
+        'betatest',
+        'releasetest',
+        'beta',
+    ),
+    '6.0.2' => array (
+        'betatest',
+        'releasetest',
+        'beta',
+    ),
+    '7.0' => array (
+        'betatest',
+        'releasetest',
+        'beta',
+    ),
+    '7.0.1' => array (
+        'betatest',
+        'releasetest',
+        'beta',
+    ),
+    '8.0' => array (
+        'betatest',
+        'releasetest',
+        'beta',
+    ),
+    '8.0.1' => array (
+        'betatest',
+        'releasetest',
+        'beta',
     ),
 );
 
