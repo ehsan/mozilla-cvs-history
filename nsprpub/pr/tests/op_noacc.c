dd2b0600df16720ecd9c57ebaf4df8489d70ee69
ted.mielczarek%gmail.com <ted.mielczarek%gmail.com>
2012-02-16 20:42:42 +0000

    bug 331299 - op_noacc.c: In function main: 94: warning: control reaches end of non-void function. Patch by Atul Aggarwal <atulagrwl@gmail.com>. r=wtc

--- nsprpub/pr/tests/op_noacc.c
+++ nsprpub/pr/tests/op_noacc.c
@@ -66,17 +66,18 @@ int main(int argc, char **argv)
     return(0);
 #endif
 
-	
+
     PR_STDIO_INIT();
-	err01 = PR_Open("err01.tmp", PR_CREATE_FILE | PR_RDWR, 0);
-	if (err01 == NULL) 
-		if (PR_GetError() == PR_NO_ACCESS_RIGHTS_ERROR) {
-				printf ("error code is %d\n",PR_GetError());
-				printf ("PASS\n");
-			return 0;
-		}
-		else {
-			printf ("FAIL\n");
-			return 1;
-		}
-}			
+    err01 = PR_Open("err01.tmp", PR_CREATE_FILE | PR_RDWR, 0);
+    if (err01 == NULL) {
+        int error = PR_GetError();
+        printf ("error code is %d\n", error);
+        if (error == PR_NO_ACCESS_RIGHTS_ERROR) {
+            printf ("PASS\n");
+            return 0;
+        }
+    }
+    printf ("FAIL\n");
+    return 1;
+}
+
