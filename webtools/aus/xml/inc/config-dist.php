b05fbb2ace42bba1720aeb42d5a4e12367995650
nrthomas%gmail.com <nrthomas%gmail.com>
2012-02-17 03:19:10 +0000

    Bug 727629, unthrottle updates to Firefox 10.0.2, r=rail

--- webtools/aus/xml/inc/config-dist.php
+++ webtools/aus/xml/inc/config-dist.php
@@ -89,20 +89,6 @@ $productThrottling = array(
      'Firefox' => array(
          '3.6.26' => 0,
          '3.6.27' => 0,
-         '4.0' => 0,
-         '4.0.1' => 0,
-         '5.0' => 0,
-         '5.0.1' => 0,
-         '6.0' => 0,
-         '6.0.1' => 0,
-         '6.0.2' => 0,
-         '7.0' => 0,
-         '7.0.1' => 0,
-         '8.0' => 0,
-         '8.0.1' => 0,
-         '9.0' => 0,
-         '9.0.1' => 0,
-         '10.0' => 0,
      )
 );
 
@@ -126,76 +112,6 @@ $throttleExceptions = array(
     '3.6.27' => array (
         'betatest',
         'releasetest'
-    ),
-    '4.0' => array (
-        'betatest',
-        'releasetest',
-        'beta',
-    ),
-    '4.0.1' => array (
-        'betatest',
-        'releasetest',
-        'beta',
-    ),
-    '5.0' => array (
-        'betatest',
-        'releasetest',
-        'beta',
-    ),
-    '5.0.1' => array (
-        'betatest',
-        'releasetest',
-        'beta',
-    ),
-    '6.0' => array (
-        'betatest',
-        'releasetest',
-        'beta',
-    ),
-    '6.0.1' => array (
-        'betatest',
-        'releasetest',
-        'beta',
-    ),
-    '6.0.2' => array (
-        'betatest',
-        'releasetest',
-        'beta',
-    ),
-    '7.0' => array (
-        'betatest',
-        'releasetest',
-        'beta',
-    ),
-    '7.0.1' => array (
-        'betatest',
-        'releasetest',
-        'beta',
-    ),
-    '8.0' => array (
-        'betatest',
-        'releasetest',
-        'beta',
-    ),
-    '8.0.1' => array (
-        'betatest',
-        'releasetest',
-        'beta',
-    ),
-    '9.0' => array (
-        'betatest',
-        'releasetest',
-        'beta',
-    ),
-    '9.0.1' => array (
-        'betatest',
-        'releasetest',
-        'beta',
-    ),
-    '10.0' => array (
-        'betatest',
-        'releasetest',
-        'beta',
     ),
 );
 
